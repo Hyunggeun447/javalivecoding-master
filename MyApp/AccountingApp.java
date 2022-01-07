974fe22222c30acdf680d8fe109d9a933b5a5b7a
bopi447 <bopi447@naver.com>
2022-01-07 16:45:04 +0900

    변수도입(리팩터링 공부)

--- MyApp/AccountingApp.java
+++ MyApp/AccountingApp.java
@@ -3,14 +3,23 @@ package MyApp;
 public class AccountingApp {
     public static void main(String[] args) {
         
-        System.out.println("Value of supply : " +12345.0);
-        System.out.println("VAT : " + (12345.0*0.1));
-        System.out.println("Total : " + (12345.0*1.1));
-        System.out.println("Expense : " + (12345.0*0.3));
-        System.out.println("Income : " + (12345.0*0.7));
-        System.out.println("Dividend : " + (12345.0*0.7)*0.5);
-        System.out.println("Dividend : " + (12345.0*0.7)*0.3);
-        System.out.println("Dividend : " + (12345.0*0.7)*0.2);
+        double valueOfSupply = 12345.0;
+        double vatRate = 0.1;
+        double vat = valueOfSupply*vatRate;
+        double total = valueOfSupply*(1+vatRate);
+        double expenseRate = 0.3;
+        double imcome = valueOfSupply*(1-expenseRate);
+        double expense = valueOfSupply*expenseRate;
+
+        System.out.println("Value of supply : " +valueOfSupply);
+        System.out.println("VAT : " + vat);
+        System.out.println("Total : " + total);
+        System.out.println("Expense : " + expense);
+        System.out.println("Income : " + imcome);
+        
+        System.out.println("Dividend : " + imcome*0.5);
+        System.out.println("Dividend : " + imcome*0.3);
+        System.out.println("Dividend : " + imcome*0.2);
 
     }
     
