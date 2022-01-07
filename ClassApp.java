ccf2e63632cc84454ea94fbb1d0406d504e4f07c
bopi447 <bopi447@naver.com>
2022-01-07 14:44:27 +0900

    인스턴스

---
+++ ClassApp.java
@@ -0,0 +1,13 @@
+import java.net.SocketPermission;
+
+import javax.print.event.PrintJobListener;
+
+public class ClassApp {
+    public static void main(String[] args) {
+        Double pi=Math.PI;
+        System.out.println(pi);
+        System.out.println(Math.ceil(pi));
+        System.out.println(Math.floor(pi));
+    }
+    
+}
