27a4767fbea5592650fa754db345bc8bc94cfc30
bopi447 <bopi447@naver.com>
2022-01-07 17:05:17 +0900

    아규먼트 사용

--- MyApp/AccountingApp.java
+++ MyApp/AccountingApp.java
@@ -3,7 +3,7 @@ package MyApp;
 public class AccountingApp {
     public static void main(String[] args) {
         
-        double valueOfSupply = 12345.0;
+        double valueOfSupply = Double.parseDouble(args[0]);
         double vatRate = 0.1;
         double vat = valueOfSupply*vatRate;
         double total = valueOfSupply*(1+vatRate);
