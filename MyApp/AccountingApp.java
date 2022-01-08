3ad82db6c40068a6628cc2ca462ba4bb6de38811
bopi447 <bopi447@naver.com>
2022-01-08 15:32:09 +0900

    배열

--- MyApp/AccountingApp.java
+++ MyApp/AccountingApp.java
@@ -10,16 +10,26 @@ public class AccountingApp {
         double expenseRate = 0.3;
         double income = valueOfSupply*(1-expenseRate);
         double expense = valueOfSupply*expenseRate;
+        
+        // 배열
+        double[] dividendRates = new double[3];
+        dividendRates[0] = 0.5;
+        dividendRates[1] = 0.3;
+        dividendRates[2] = 0.2;
 
+        double dividend1 = income*dividendRates[0];
+        double dividend2 = income*dividendRates[1];
+        double dividend3 = income*dividendRates[2];
+        
+        
         System.out.println("Value of supply : " +valueOfSupply);
         System.out.println("VAT : " + vat);
         System.out.println("Total : " + total);
         System.out.println("Expense : " + expense);
         System.out.println("Income : " + income);
-        
-        System.out.println("Dividend 1 : " + income*0.5);
-        System.out.println("Dividend 2 : " + income*0.3);
-        System.out.println("Dividend 3 : " + income*0.2);
+        System.out.println("Dividend 1 : " + dividend1);
+        System.out.println("Dividend 2 : " + dividend2);
+        System.out.println("Dividend 3 : " + dividend3);
 
     }
     
