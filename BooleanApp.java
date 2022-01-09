468dc0653904b44de735ac1bbbf2329e92067e42
bopi447 <bopi447@naver.com>
2022-01-09 14:02:07 +0900

    boolean 데이터타입

---
+++ BooleanApp.java
@@ -0,0 +1,17 @@
+public class BooleanApp {
+    public static void main(String[] args) {
+        System.out.println("ONE");
+        System.out.println(1);
+
+        System.out.println(true);
+        System.out.println(false+"\n");
+        
+        String foo = "Hello World";
+        // String true = "Hello World";  -> reserved word. (예약어)
+
+        System.out.println(foo.contains("world"));
+        System.out.println(foo.contains("wikiki"));
+        
+        
+    }
+}
