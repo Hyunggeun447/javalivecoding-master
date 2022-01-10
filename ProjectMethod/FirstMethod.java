fe4cddd874434fbb4dc486403593dd24298028e2
bopi447 <bopi447@naver.com>
2022-01-10 14:11:20 +0900

    메소드 입력값 추가

--- ProjectMethod/FirstMethod.java
+++ ProjectMethod/FirstMethod.java
@@ -2,15 +2,15 @@ package ProjectMethod;
 
 public class FirstMethod {
     
-    private static void printTwoTimes(String text) {
-        System.out.println("-");
+    private static void printTwoTimes(String text, String delimiter) {
+        System.out.println(delimiter);
         System.out.println(text);
         
     }
     public static void main(String[] args) {
         
         for(int i=0; i<10; i++){
-        printTwoTimes("A");
+        printTwoTimes("A", "*********");
         }
         
         
