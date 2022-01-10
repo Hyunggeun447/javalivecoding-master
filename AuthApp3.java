3b709ee78480df817b8c933f0abc873d77a7fc2a
bopi447 <bopi447@naver.com>
2022-01-10 13:25:08 +0900

    2차원 배열을 통한 조건,반복문

--- AuthApp3.java
+++ AuthApp3.java
@@ -1,16 +1,28 @@
 public class AuthApp3 {
     public static void main(String[] args) {
         
-        String[] users = {"egoing","jinhuck","youbin"};
+        // String[] users = {"egoing","jinhuck","youbin"};
+        // 2차원 배열
+        String[][] users = {
+            {"egoing","1111"},
+            {"jinhuck","2222"},
+            {"youbin","3333"}
+        };
+
 
         String inputId = args[0];
+        String inputPass = args[1];
 
         boolean isLogined = false;
         for(int i=0; i<users.length; i++){
             // System.out.println(users[i]);
-            String currentId = users[i];
+            String currentId = users[i][0];
+            String currentPass = users[i][1];
 
-            if(currentId.equals(inputId)){
+            if(
+                currentId.equals(inputId) &&
+                currentPass.equals(inputPass)
+            ){
                 isLogined = true;
                 break;
             }
@@ -18,7 +30,6 @@ public class AuthApp3 {
         System.out.println("Hi");
         if(isLogined){
             System.out.println("Master!");
-
         } else{
             System.out.println("who are you?");
         }
