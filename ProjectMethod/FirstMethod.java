461b909cede78e3c75f338a0858ca0525bef42eb
bopi447 <bopi447@naver.com>
2022-01-10 14:09:42 +0900

    메소드를 좀더 효율적으로 변환

--- ProjectMethod/FirstMethod.java
+++ ProjectMethod/FirstMethod.java
@@ -2,15 +2,15 @@ package ProjectMethod;
 
 public class FirstMethod {
     
-    private static void printTwoTimesA() {
+    private static void printTwoTimes(String text) {
         System.out.println("-");
-        System.out.println("A");
+        System.out.println(text);
         
     }
     public static void main(String[] args) {
         
         for(int i=0; i<10; i++){
-        printTwoTimesA();
+        printTwoTimes("A");
         }
         
         
