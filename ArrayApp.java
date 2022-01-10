2a9a1bf3992288867695bffc298463c777de7247
bopi447 <bopi447@naver.com>
2022-01-10 12:58:51 +0900

    array 만들기

---
+++ ArrayApp.java
@@ -0,0 +1,20 @@
+public class ArrayApp {
+    public static void main(String[] args) {
+        
+        // String[] users = new String[3];
+        // users[0] = "egoing";
+        // users[1] = "jinhuck";
+        // users[2] = "youbin";
+        
+
+        String[] users = {"egoing","jinhuck","youbin"};
+
+        System.out.println(users[2]);
+        System.out.println(users.length);
+
+        int[] scores = {10,20,30};
+        System.out.println(scores[1]);
+        System.out.println(scores.length);
+    }
+    
+}
