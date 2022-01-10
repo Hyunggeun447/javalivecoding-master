f51cca523c8c549ec3d01ffc4102167fd6f48c6e
bopi447 <bopi447@naver.com>
2022-01-10 17:38:43 +0900

    클레스를 다른 파일로 분산함으로써 더욱 간단히 보이도록 정리할 수 있다. 다만 리펙터링 오류가 뜸 why?

--- ProjectOOP/MyOOP.java
+++ ProjectOOP/MyOOP.java
@@ -1,17 +1,6 @@
 package ProjectOOP;
 
-class print{
-    public static String divided = "";
 
-    public static void word3(String text) {
-        System.out.println(divided);
-        System.out.println(text);
-        System.out.println(text);
-        System.out.println(text);
-
-    }
-
-}
 public class MyOOP {
     public static void main(String[] args) {
         print.divided = "*******";
---
+++ ProjectOOP/print.java
@@ -0,0 +1,15 @@
+package ProjectOOP;
+
+public class print {
+    public static String divided = "";
+
+    public static void word3(String text) {
+        System.out.println(divided);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+
+    }
+
+    
+}
