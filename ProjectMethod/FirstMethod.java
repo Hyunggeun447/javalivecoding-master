701037f7a0ef367f944699b03f022867212b71c2
bopi447 <bopi447@naver.com>
2022-01-10 14:06:09 +0900

    메소드의 기본형식

---
+++ ProjectMethod/FirstMethod.java
@@ -0,0 +1,22 @@
+package ProjectMethod;
+
+public class FirstMethod {
+    
+    private static void printTwoTimesA() {
+        System.out.println("-");
+        System.out.println("A");
+        
+    }
+    public static void main(String[] args) {
+        
+        for(int i=0; i<10; i++){
+        printTwoTimesA();
+        }
+        
+        
+        // .....
+    }
+
+    
+    
+}
