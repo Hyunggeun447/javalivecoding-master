92bf1604097a3755d7d04d5fb8310c856d8d3342
bopi447 <bopi447@naver.com>
2022-01-10 18:08:37 +0900

    인스턴스

--- ProjectOOP/MyOOP.java
+++ ProjectOOP/MyOOP.java
@@ -3,11 +3,26 @@ package ProjectOOP;
 
 public class MyOOP {
     public static void main(String[] args) {
-        print.divided = "*******";
-        print.word3("I love you");
+        // 클래스명 인스턴스명(생성) = new 클래스명;
+        // 인스턴스1.메소드 = ~~1;
+        // 인스턴스2.메소드 = ~~2;
+        // 각 인스턴스마다 메소드를 따로 저장할 수 있고 그로 인해서 좀더 간결하게 코딩할 수 있다.
+        print p1 = new print();
+        p1.divided = "*******";
+        p1.word3("asdf");
 
-        print.divided ="////////";
-        print.word3("java");
+        print p2 = new print();
+        p2.divided = "======";
+        p2.word3("java");
+
+        p1.reversed3("abcdef");
+        p2.reversed3("1234567");
+
+
+        // print.word3("I love you");
+
+        // print.divided ="////////";
+        // print.word3("java");
         
     }
 
--- ProjectOOP/print.java
+++ ProjectOOP/print.java
@@ -1,15 +1,26 @@
 package ProjectOOP;
 
 public class print {
-    public static String divided = "";
+    public  String divided = "";
 
-    public static void word3(String text) {
+    public void word3(String text) {
         System.out.println(divided);
         System.out.println(text);
         System.out.println(text);
         System.out.println(text);
+    }
+
+    public void reversed3(String text){
+        System.out.println(divided);
+        StringBuffer txt2 = new StringBuffer(text);
+        System.out.println(txt2.reverse().toString());
+        System.out.println(txt2);
+        System.out.println(txt2);
+
 
     }
+
+
 
     
 }
