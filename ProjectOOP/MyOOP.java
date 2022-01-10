50f82743f87abdbb7965774044dbc1dbf3f7fa4d
bopi447 <bopi447@naver.com>
2022-01-10 17:08:58 +0900

    객체지향_변수와 메소드

---
+++ ProjectOOP/MyOOP.java
@@ -0,0 +1,24 @@
+package ProjectOOP;
+
+public class MyOOP {
+    public static void main(String[] args) {
+        //divided = "*******";
+        
+        print3Word("I love you");
+
+        divided ="////////";
+        print3Word("java");
+    }
+
+    //===================================================
+    public static String divided = "";
+
+    public static void print3Word(String text) {
+        System.out.println(divided);
+        System.out.println(text);
+        System.out.println(text);
+        System.out.println(text);
+
+    }
+    
+}
