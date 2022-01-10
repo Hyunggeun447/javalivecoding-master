4993c198db29425675b17d5530406b7fcb45c41b
bopi447 <bopi447@naver.com>
2022-01-10 19:48:04 +0900

    객체지향:생성자(인스턴스 초기설정), this(현재 인스턴스를 가리키는)

--- ProjectOOP/MyOOP.java
+++ ProjectOOP/MyOOP.java
@@ -7,14 +7,15 @@ public class MyOOP {
         // 인스턴스1.메소드 = ~~1;
         // 인스턴스2.메소드 = ~~2;
         // 각 인스턴스마다 메소드를 따로 저장할 수 있고 그로 인해서 좀더 간결하게 코딩할 수 있다.
-        print p1 = new print();
-        p1.divided = "*******";
+        print p1 = new print("----");
+        
         p1.word3("asdf");
 
-        print p2 = new print();
-        p2.divided = "======";
+        print p2 = new print("***********");
+        
         p2.word3("java");
 
+        p1.divided=".........";
         p1.reversed3("abcdef");
         p2.reversed3("1234567");
 
--- ProjectOOP/print.java
+++ ProjectOOP/print.java
@@ -1,17 +1,21 @@
 package ProjectOOP;
 
 public class print {
+    public print(String divided) {
+        this.divided = divided;
+
+    }
     public  String divided = "";
 
     public void word3(String text) {
-        System.out.println(divided);
+        System.out.println(this.divided);
         System.out.println(text);
         System.out.println(text);
         System.out.println(text);
     }
 
     public void reversed3(String text){
-        System.out.println(divided);
+        System.out.println(this.divided);
         StringBuffer txt2 = new StringBuffer(text);
         System.out.println(txt2.reverse().toString());
         System.out.println(txt2);
