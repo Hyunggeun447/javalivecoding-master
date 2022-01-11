896856d6a5e955e61c7ce4b26f01611498c62ee1
bopi447 <bopi447@naver.com>
2022-01-11 14:01:19 +0900

    상속:overloading

--- Inheritance/InheritanceApp.java
+++ Inheritance/InheritanceApp.java
@@ -4,6 +4,11 @@ class cal{
     public int sum(int v1 , int v2){
         return v1+v2;
     }
+
+    // overloading
+    public int sum(int v1 , int v2, int v3){
+        return v1+v2+v3;
+    }
 }
 
 class cal3 extends cal{
@@ -17,13 +22,18 @@ class cal3 extends cal{
         return v1+v2;
     }
 
+    // 상속받은 클래스에서도 overloading 역시 가능.  if cal sum (v1,v2,v3) 가 없다면.
+    // public int sum(int v1 , int v2, int v3){
+    //     return v1+v2+v3;
+
 }
 
 public class InheritanceApp {
     public static void main(String[] args) {
         cal c = new cal();
         System.out.println(c.sum(2, 1));
-        
+        System.out.println(c.sum(2, 1, 3));
+
         cal3 d = new cal3();
         System.out.println(d.minus(10, 2));
         System.out.println(d.sum(10, 2)); // cal3 는 cal로부터 "상속"받아 sum 메소드를 사용할 수 있다.
