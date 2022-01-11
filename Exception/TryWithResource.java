9c96bc29e787f1cb9bd55b35223836d20b5564db
bopi447 <bopi447@naver.com>
2022-01-11 18:58:31 +0900

    예외: 리소스를 사용할 때는 이 구문을 사용하는게 더 간편하다

---
+++ Exception/TryWithResource.java
@@ -0,0 +1,15 @@
+package Exception;
+
+import java.io.FileWriter;
+
+public class TryWithResource {
+    public static void main(String[] args) {
+        // try with resource statements
+        try(FileWriter f = new FileWriter("text21.txt")){
+            f.write("뭐가문제야");
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+}
